fc03d0e9ae1b9baf5fe17a92044a34c373db110b
AnastasiaZhekina <yfcnzjkzdbnz09@gmail.com>
2023-06-16 22:34:45 +0500

    Добавьте файлы проекта.

---
+++ LambdaFunction/LambdaFunction.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <algorithm>
+
+int main() {
+
+    std::vector<int> arr = { 4, 7, 9, 14, 12 };
+
+    std::for_each(arr.begin(), arr.end(), [](int& num) {
+        if (num % 2 != 0) {
+            num *= 3;
+        }});
+
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
